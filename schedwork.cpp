d89386d8d4296a9822e5cec4410956b61e4b9c4e
“Jushen <jsdhillo@usc.edu>
2025-04-22 16:36:43 -0700

    Ready for codio testing

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,25 +21,71 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool isValid(DailySchedule& currentSched, const size_t maxShifts, int row, int col, vector<int>& workersShifts);
+bool buildScheduleByRow(std::vector<int> todaysWorkers, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int rowNumber, int colNumber);
 
 
 // Add your implementation of schedule() and other helper functions here
+bool buildScheduleByRow(std::vector<int> todaysWorkers, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int rowNumber, int colNumber) {
+	if (colNumber == dailyNeed) {
+		std::vector<int> workersShifts(sched[0].size());
+		if (isValid(sched, maxShifts, 0, 0, workersShifts)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	else {
+		for (int i = 0; i < todaysWorkers.size(); i++) {
+			sched[rowNumber][colNumber] = todaysWorkers[i];
+			std::vector<int> workersShifts(sched[0].size());
+			if (isValid(sched, maxShifts, 0, 0, workersShifts)) {
+				if (colNumber + 1 <= dailyNeed) {
+					buildScheduleByRow(todaysWorkers, dailyNeed, maxShifts, sched, rowNumber, colNumber + 1);
+				}
+			}
+			else {
+				sched[rowNumber][colNumber] = INVALID_ID;
+			}
+		}
+	}
+}
 
-bool schedule(
-    const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
-)
-{
-    if(avail.size() == 0U){
+//recursively checks if currentSched is a valid schedule
+bool isValid(DailySchedule& currentSched, const size_t maxShifts, int row, int col, vector<int>& workersShifts) {
+	int workerID = currentSched[row][col];
+	workersShifts[workerID]++;
+	if (workersShifts[workerID] > maxShifts) {
+		return false;
+	}
+	else {
+		if (col + 1 < currentSched[0].size()) {
+			return isValid(currentSched, maxShifts, row, col + 1, workersShifts);
+		}
+		else {
+			if (row + 1 < currentSched.size()) {
+				return isValid(currentSched, maxShifts, row + 1, 0, workersShifts);
+			}
+			return true;
+		}
+	}
+}
+
+bool schedule(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched) {
+    if(avail.size() == 0){
         return false;
     }
     sched.clear();
     // Add your code below
 
-
-
-
+	int kValue = avail[0].size();
+	int nValue = avail.size();
+	for (int i = 0; i < nValue; i++) {
+		std::vector<int> todaysWorkers;
+		for (int j = 0; j < kValue; j++) {
+			todaysWorkers.push_back(avail[i][j]);
+		}
+		buildScheduleByRow(todaysWorkers, dailyNeed, maxShifts, sched, i, 0);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+bool isValidWordle(const std::string& in, std::string& current, const std::string& floating, const std::set<std::string>& dict);
+std::set<std::string> wordleHelper(const std::string& in, std::string& current, const std::string& floating, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,57 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+	std::string current = "";
+	return wordleHelper(in, current, floating, dict);
 
 }
 
 // Define any helper functions here
+
+std::set<std::string> wordleHelper(const std::string& in, std::string& current, const std::string& floating, const std::set<std::string>& dict) {
+	
+	if (isValidWordle(in, current, floating, dict)) {
+		std::set<std::string> validWord;
+		validWord.insert(current);
+		return validWord;
+	}
+	else {
+		std::set<std::string> validWord;
+		std::set<std::string> allValidWords;
+		int pos = current.length();
+		if (in[pos] != '-') {
+			std::string newIn = in;
+			newIn[pos] = '-';
+			std::string newCurrent = current + in[pos];
+			return wordleHelper(newIn, newCurrent, floating, dict);
+		}
+		else {
+			// Open position ('-')
+			std::set<std::string> allValidWords;
+			for (int i = 0; i < 26; i++) {
+				std::set<std::string> validWord = wordleHelper(in, current + alphabet[i], floating, dict);
+				// Merge validWord into allValidWords
+			}
+			return allValidWords;
+		}
+	}
+}
+
+bool isValidWordle(const std::string& in, std::string& current, const std::string& floating, const std::set<std::string>& dict) {
+	std::string currentCopy = current;
+	if (in.length() == currentCopy.length()) {
+		for (int i = 0; i < floating.length(); i++) {
+			if (currentCopy.find(floating[i]) != string::npos) {
+				int index = currentCopy.find(floating[i]);
+				currentCopy[index] = '-';
+			}
+			else {
+				return false;
+			}
+		}
+		if (/*current (not currentCopy) is a word in dict*/) {
+			return true;
+		}
+	}
+	return false;
+};
